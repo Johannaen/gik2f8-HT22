e3399061f10833f06461d95d57c71e1c368f1a92
Johanna Engdahl Nilsson <johanna.en@hotmail.com>
2022-11-16 20:12:52 +0100

    genererar html-lista

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ const bookList = [
 ];
 
 /*const searchInput = document.children[0].children[1].children[1].children[1]; */
-const searchField = document.getElementById('searchField');
-console.log(searchField);
+/*const searchField = document.getElementById('searchField');
+console.log(searchField);*/
 
 /* keydown */ 
 searchField.addEventListener("keyup", handleKeyPress);
@@ -49,8 +49,30 @@ function searchBooks(searchTerm){
 }
 
 
-function renderBookList(list){
+function renderBookList(bookList){
     /*element i HTML-listan visas/döljs beroende på listans innehåll */ 
-    console.log(list);
+    let html = `<ul class="book-list rounded-md border-2 border-black bg-white w-full mx-auto">`;
+        for(let i = 0; i < bookList.length; i++){
+           html += `<li
+                        class="book-list__item mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b border-indigo-700 cursor-pointer">
+                        ${bookList[i].author} - ${bookList[i].title}
+                    </li>` 
+        }
+
+
+    html +=    `</ul> `;
+
+    const existingElement = document.querySelector(".book-list");
+    console.log(existingElement);
+
+    const root = document.getElementById("root");
+    if(existingElement){
+        root.removeChild(existingElement);
+    }
+
+    root.insertAdjacentHTML("beforeend", html);
+
+
+    
 }
 
