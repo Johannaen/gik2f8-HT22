a284ef5ffaf45573a8d8becb1bae1d020118091c
Johanna Engdahl Nilsson <johanna.en@hotmail.com>
2022-11-16 19:22:02 +0100

    buggfix

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function searchBooks(searchTerm){
     for(let i = 0; i < bookList.length; i++) {
         const title = bookList [i].title.toLowerCase();
         if(title.indexOf(searchTerm.toLowerCase()) >=  0) {
-        filteredList.push(bookList[0]); 
+        filteredList.push(bookList[i]); 
     }
     }
     renderBookList(filteredList);
@@ -49,4 +49,4 @@ function renderBookList(list){
     console.log(list);
 }
 
-handleKeyPress('e');
+handleKeyPress('Ham');
